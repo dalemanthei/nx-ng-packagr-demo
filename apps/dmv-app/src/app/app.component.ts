10dc11d1e8a4f78a403b707715f62d8d42ab0d9c
Dale Manthei <dalemanthei@users.noreply.github.com>
2020-01-24 14:19:04 -0700

    initial commit

---
+++ apps/dmv-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dmv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dmv-app';
+}
